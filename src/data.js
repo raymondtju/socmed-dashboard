515bc8747714fa364a282353caa0e04233e03577
raymondtju <raymond.rtju@gmail.com>
2023-02-03 00:07:03 +0700

    test

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const datas = {
   data: [
     {
-      icon: "/../public/images/facebook.png",
+      icon: "/public/images/facebook.png",
       name: "facebook",
       username: "nathanf",
       type: "followers",
