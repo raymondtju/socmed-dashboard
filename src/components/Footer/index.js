1d97a649adda2ea426cae196d720c7389bb0c434
raymondtju <raymond.rtju@gmail.com>
2023-02-02 23:55:12 +0700

    create Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import datas from "@/data";
+import Theme from "../Theme";
+
+export default function Footer() {
+  return (
+    <footer className="mt-4 mb-4">
+      <p className="text-sm font-semibold text-center text-slate-500">
+        Code by @Ferropayo.
+      </p>
+    </footer>
+  );
+}
